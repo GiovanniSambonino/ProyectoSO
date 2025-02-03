7992a10f9ce29dc324c85185865a15e61769c995
ItsDiegoTBG <diealcon@espol.edu.ec>
2025-02-03 14:10:20 -0500

    Git Merge con rama gsamboni

--- pa3_MultiHilos_SO/filter.h
+++
@@ -1,7 +0,0 @@
-#include "bmp.h"
-
-void apply(BMP_Image * imageIn, BMP_Image * imageOut);
-
-void applyParallel(BMP_Image * imageIn, BMP_Image * imageOut, int boxFilter[3][3], int numThreads);
-
-void *filterThreadWorker(void * args);
